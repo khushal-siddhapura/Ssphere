2d91d1a0a17520130529df4db309cbdd71b06876
khushal-siddhapura <khushalconnect@gmail.com>
2025-09-05 22:31:20 +0530

    CORS Origin Error resolve

--- Server/server.js
+++ Server/server.js
@@ -37,7 +37,7 @@ mongoose
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://ssphere-e-commerce-company.vercel.app/auth/login",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
