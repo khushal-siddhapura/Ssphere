7afbef60cbb255ab9fb5f4b7138dd1267467eae9
khushal-siddhapura <khushalconnect@gmail.com>
2025-09-10 00:38:11 +0530

    Resolve cors origin Routes

--- Server/server.js
+++ Server/server.js
@@ -37,7 +37,7 @@ mongoose
 
 app.use(
   cors({
-    origin: "https://ssphere-e-commerce-company.vercel.app/shop/home",
+    origin: "https://ssphere-e-commerce-company.vercel.app",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
