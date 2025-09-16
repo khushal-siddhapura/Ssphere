663fb2c2d655a895f858aaa70b569a7b1b24ba42
khushal-siddhapura <khushalconnect@gmail.com>
2025-09-16 11:17:36 +0530

    Solve Import typo

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -30,9 +30,10 @@ import ComplaintPanel from "./pages/shopping-view/ComplaintPanel";
 import ResetPassword from "./pages/auth/ResetPassword";
 import ForgotPassword from "./pages/auth/ForgotPassword";
 import ComplainView from "./pages/admin-view/ComplainView";
-import RevenueData from "./pages/admin-view/RevenueData.jsx";
+// import RevenueData from "./pages/admin-view/RevenueData.jsx";
 import AdminList from "./pages/admin-view/AdminList";
 import UserManagement from "./components/admin-view/UserManagement";
+import RevenueData from "./pages/admin-view/RevenueData";
 // import PrivateRoute from "./components/PrivateRoute";  // import PrivateRoute component
 
 const PrivateRoute = ({ isAuthenticated, children }) => {
--- TODO.md
+++
@@ -1,7 +0,0 @@
-# TODO: Fix CORS Issue in E-commerce App
-
-## Tasks
-- [x] Edit Server/server.js to change origin from static string to true in cors middleware
-- [x] Edit Server/server.js to change origin from static string to true in app.options cors
-- [ ] Redeploy the server to apply changes
-- [ ] Test frontend login and auth check requests
