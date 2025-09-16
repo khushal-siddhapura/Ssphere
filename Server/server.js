2b916ef56a428ac0cc47f9876eb022a0e9cb9945
khushal-siddhapura <khushalconnect@gmail.com>
2025-09-16 18:41:20 +0530

    Correct Cors policy code from server.js

--- Server/server.js
+++ Server/server.js
@@ -35,24 +35,24 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.log(error));
 
-// const allowedOrigins = ["https://ssphere-e-commerce-company.vercel.app"];
+const allowedOrigins = ["https://ssphere-e-commerce-company.vercel.app"];
 
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       // allow requests with no origin (like mobile apps or curl requests)
-//       if (!origin) return callback(null, true);
-//       if (allowedOrigins.indexOf(origin) === -1) {
-//         const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
-//         return callback(new Error(msg), false);
-//       }
-//       return callback(null, true);
-//     },
-//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 // Handle preflight requests for all routes
 app.options("*", cors());
